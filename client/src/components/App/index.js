a864e189125c0531369d605dc02c4a70590a14c9
Евгений Якушев <EVGYAK@live.spbstu.ru>
2019-01-16 02:13:49 +0300

    show source icon

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -9,6 +9,7 @@ const API_URL_SEARCH = 'api/esearch';
 class App extends React.Component {
     render() {
         const template = item => <li key={item._id}>
+            <img src={new URL(item.url).origin + '/favicon.ico'} title={item.sourceId} />
             <a href={item.url} target='_blank'>{item.title}</a>
             <span dangerouslySetInnerHTML={{__html: item.description}}></span>
         </li>
