4d974b7100c1849017d3c0706ae04c1bfe732f51
Yevgeny Yakushev <e.v.yakushev@gmail.com>
2019-04-03 00:29:58 +0300

    search api refactoring

--- api/esearch.js
+++ api/esearch.js
@@ -1,35 +1,56 @@
 const db = require('../db/').elasticsearch;
 
+async function _search(query = '') {
+    const data = await db.search({
+        index: 'edu',
+        body: {
+            query: {
+                multi_match: {
+                    query,
+                    fuzziness: 1,
+                    fields: [
+                        'title^3',
+                        'url^2',
+                        'description^1'
+                    ]
+                }
+            },
+            size: 10000,
+            sort: [
+                { lang: { order: 'desc' }},
+                { _score: { order: 'desc' }},
+            ]
+        }
+    });
+
+    return { q: query, data: data.hits.hits.map(({ 
+        _id, 
+        _source: { 
+            title, 
+            url, 
+            description, 
+            sourceId, 
+            dateFrom, 
+            rating 
+        } = {}
+    }) => ({
+        _id, 
+        title, 
+        url, 
+        description, 
+        sourceId, 
+        dateFrom, 
+        rating
+    })) };
+}
+
 module.exports = app => {
     app.get('/api/esearch', async function (req, res) {
-        const { q = '' } = req.query;
-
-        const data = await db.search({
-            index: 'edu',
-            body: {
-                query: {
-                    multi_match: {
-                        query: q,
-                        fuzziness: 1,
-                        fields: [
-                            'title^3',
-                            'url^2',
-                            'description^1'
-                        ]
-                    }
-                },
-                size: 10000,
-                sort: [
-                    { lang: { order: 'desc' }},
-                    { _score: { order: 'desc' }},
-                ]
-            }
-        });
-
-        res.send({ q, data: data.hits.hits.map(({ 
-            _id, _source: { title, url, description, sourceId, dateFrom, rating } = {}
-        }) => ({
-            _id, title, url, description, sourceId, dateFrom, rating
-        })) });
+        const data = await _search(req.query.q)
+        res.send(data);
     });
+
+    return {
+        search: _search
+    }
 };
