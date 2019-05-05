e5d2f2debf9273a5e0de7064b5bf6f7f4c2c846a
Евгений Якушев <EVGYAK@live.spbstu.ru>
2019-05-05 05:33:11 +0300

    disable mongodb for development; show starting info

--- db/index.js
+++ db/index.js
@@ -1,17 +1,19 @@
 const config = require('../config');
 
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
-mongoose.connect(config['db.mongo.url'], {
-    useNewUrlParser: true
-});
+let db = {};
 
-const elasticsearch = new require('elasticsearch').Client({
+if (process.env.NODE_ENV === 'production') {
+    const mongoose = require('mongoose');
+    mongoose.Promise = Promise;
+    mongoose.connect(config['db.mongo.url'], {
+        useNewUrlParser: true
+    });
+    db.mongoose = mongoose;
+}
+
+db.elasticsearch = new require('elasticsearch').Client({
     host: config['db.elastic.url'],
     requestTimeout: 60000
 });
 
-module.exports = {
-    mongoose: mongoose,
-    elasticsearch: elasticsearch
-};
+module.exports = db;
